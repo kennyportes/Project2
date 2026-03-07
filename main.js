2d914ada9183f2e8cb7555ad276736173aaa06a3
Kenny Portes <porteskenny@gmail.com>
2026-03-07 00:25:08 -0500

    I started updating my JavaScript

--- main.js
+++ main.js
@@ -1,8 +1,22 @@
+let searchForm = document.getElementById('form');
 
-fetch('https://api.rawg.io/api/games?key=a798828782a44a61b7c563c58c97e47f&platforms=187')
-.then(response => response.json())
-.then(gaming =>{
-    displaygame(gaming.data);
-});
+let results = document.getElementById('outputs');
+
+
+
+
+
+
+
+
+
+
+
+
+// fetch('https://api.rawg.io/api/games?key=a798828782a44a61b7c563c58c97e47f&platforms=187')
+// .then(response => response.json())
+// .then(gaming =>{
+//     displaygame(gaming.data);
+// });
 
 
