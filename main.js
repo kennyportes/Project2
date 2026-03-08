bdeeb0eb2485cd608efea676329602727d479f5f
Kenny Portes <porteskenny@gmail.com>
2026-03-08 04:42:08 -0400

    JavaScript code did not work I am redoing it.

--- main.js
+++ main.js
@@ -1,26 +1,3 @@
-let searchForm = document.getElementById('inputform');
-
-let results = document.getElementById('outputs');
-
-searchForm.addEventListener("submit", function(form){
-    form.preventDefault();
-
-    let text = document.getElementById('inputs').value;
-
-    if (text === "") {
-        alert("Please Enter a Text to Search");
-        return;
-    }
-    results.innerHTML = "Loading...";
-
-    fetch("https://api.rawg.io/api/games?key=a798828782a44a61b7c563c58c97e47f&platforms=187")
-    .then(res => {res.json()
-    })
-
-    
-
-})
-
 
 
 
